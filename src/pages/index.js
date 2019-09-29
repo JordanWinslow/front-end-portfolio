5fd02793a3bb7234ad29b5b4358294f36952536d
Jordan Winslow <admin@jordanwinslow.me>
2019-09-28 21:03:22 -0700

    Added SSR workaround for React.lazy

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,41 +7,44 @@ const PostTitle = React.lazy(() => import("../components/PostTitle"))
 const BlogPost = React.lazy(() => import("../components/BlogPost"))
 
 const IndexPage = () => {
+  const isServerRendered = typeof window === "undefined"
   return (
     <Layout>
       <SEO title="Home" />
-      <Suspense
-        fallback={
-          <div
-            style={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-            }}
-          >
-            <img src={Loading} />
-          </div>
-        }
-      >
-        <PostTitle
-          heading="FRONT-END WEB DEVELOPMENT BLOG"
-          subHeading="JavaScript, React, GraphQL & Friends"
-        />
-        {/*<BlogList />*/}
-        <BlogPost
-          content={
-            /*TODO: Replace content with dynamically loaded markdown files */
-            <div>
-              BOOM. Dark mode toggle implemented. Now you can click the
-              lightbulb to switch color schemes along with a fading animation.
-              <br />
-              <br />
-              I'm balling out of control.
+      {!isServerRendered && (
+        <Suspense
+          fallback={
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+              }}
+            >
+              <img src={Loading} />
             </div>
           }
-        />
-      </Suspense>
+        >
+          <PostTitle
+            heading="FRONT-END WEB DEVELOPMENT BLOG"
+            subHeading="JavaScript, React, GraphQL & Friends"
+          />
+          {/*<BlogList />*/}
+          <BlogPost
+            content={
+              /*TODO: Replace content with dynamically loaded markdown files */
+              <div>
+                BOOM. Dark mode toggle implemented. Now you can click the
+                lightbulb to switch color schemes along with a fading animation.
+                <br />
+                <br />
+                I'm balling out of control.
+              </div>
+            }
+          />
+        </Suspense>
+      )}
       {/*<div className="bgPrimaryLight">This is a success message</div>
     <div className="bgSecondaryLight">This is a warning message</div>*/}
     </Layout>
