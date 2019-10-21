efe262e217b39b34069a68f7379d55f815f08f4f
JordanWinslow <admin@jordanwinslow.me>
2019-10-21 16:11:47 -0700

    updated SEO title

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -43,7 +43,7 @@ function SEO({ description, image, lang, meta, title }) {
         },
         {
           property: `og:title`,
-          content: title,
+          content: `${title} | ${site.siteMetadata.title}`,
         },
         {
           property: `og:description`,
@@ -67,7 +67,7 @@ function SEO({ description, image, lang, meta, title }) {
         },
         {
           name: `twitter:title`,
-          content: title,
+          content: `${title} | ${site.siteMetadata.title}`,
         },
         {
           name: `twitter:description`,
